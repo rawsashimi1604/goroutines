009aef74319b40798ebe47ae7d898f23def23767
rawsashimi1604 <looweiren@gmail.com>
2023-08-24 18:01:05 +0800

    added lesson 1

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Print("hello world")
+	runGoroutineLesson1()
+}
+
+func runGoroutineLesson1() {
+	go numbers()
+	go alphabets()
+	time.Sleep(3000 * time.Millisecond)
+	fmt.Println("terminated from main")
+}
+
+func sayHello() {
+	fmt.Println("Hello world")
+}
+
+func numbers() {
+	for i := 1; i <= 5; i++ {
+		time.Sleep(250 * time.Millisecond)
+		fmt.Printf("%d ", i)
+	}
+}
+func alphabets() {
+	for i := 'a'; i <= 'e'; i++ {
+		time.Sleep(400 * time.Millisecond)
+		fmt.Printf("%c ", i)
+	}
 }
